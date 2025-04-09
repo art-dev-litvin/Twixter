0edb377d461d23056b54c00f7e7b2b13c9bc6e84
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-04-09 13:32:49 +0300

    chore: remove firestore configuration

--- server/src/firestore/firestore.providers.ts
+++
@@ -1,7 +0,0 @@
-import { UserDocument } from 'src/auth/auth.document';
-
-export const FirestoreDatabaseProvider = 'firestoredb';
-export const FirestoreOptionsProvider = 'firestoreOptions';
-export const FirestoreCollectionProviders: string[] = [
-  UserDocument.collectionName,
-];
