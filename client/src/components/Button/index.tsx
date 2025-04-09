565374f7fd6b32301b92ca3bb8c01495b2f63032
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-04-09 17:06:03 +0300

    refactor: use hover button effect if not disabled

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -26,16 +26,18 @@ const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const classNamesCombined = classNames(
-    "h-11 px-3 border rounded-md cursor-pointer transition-colors duration-200 inline-flex items-center justify-center",
+    "h-11 px-3 border rounded-md cursor-pointer transition-colors duration-200 inline-flex items-center justify-center ",
     {
-      "bg-emerald-500 hover:bg-emerald-600 py-2 px-4 text-white font-medium":
+      "bg-emerald-500 hover:[&:not(:disabled)]:bg-emerald-600 py-2 px-4 text-white font-medium":
         variant === "green",
-      "bg-slate-500 text-white hover:bg-slate-600": variant === "gray",
-      "bg-red-500 text-white hover:bg-red-600": variant === "destructive",
-      "border-red-500 text-red-500 hover:bg-red-50":
+      "bg-slate-500 text-white hover:[&:not(:disabled)]:bg-slate-600":
+        variant === "gray",
+      "bg-red-500 text-white hover:[&:not(:disabled)]:bg-red-600":
+        variant === "destructive",
+      "border-red-500 text-red-500 hover:[&:not(:disabled)]:bg-red-50":
         variant === "destructiveOutlined",
       "w-full": fullWidth,
-      "bg-gray-400 cursor-not-allowed": (props as any).disabled,
+      "bg-gray-400 !cursor-not-allowed": (props as any).disabled,
     },
     className
   );
